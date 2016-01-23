715a2c9cfda82a9fec784b39f0ca6745491f86b2
Mario <mcarrillo.05@hotmail.com>
2016-01-23 11:14:48 -0600

    conector

---
+++ src/kentia/modelo/conector.go
@@ -0,0 +1,27 @@
+package modelo
+
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+)
+
+//Conector Go -> Mongo que permite obtener sesiones en una bd.
+type conector struct {
+	db *mgo.Database
+}
+
+// Realiza una conexión al servidor server y selecciona la BD de kentia
+func (c *conector) IniciarSesion() {
+	session, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		log.Println(err)
+	}
+	session.SetSafe(&mgo.Safe{})
+	c.db = session.DB("kentia")
+}
+
+// Finaliza la conexión al servidor
+func (c *conector) CerrarSesion() {
+	c.db.Logout()
+}
