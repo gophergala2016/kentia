545c10bb2635dd05efc3bcaeef8ffd971fd3e269
Carlos <remnanttime@gmail.com>
2016-01-23 13:01:21 -0600

    sd

---
+++ src/kentia/modelo/usuario.go
@@ -0,0 +1,14 @@
+package modelo
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"kentia/log"
+)
+
+type Usuario struct {
+	ID         bson.ObjectId `bson:"_id"`
+	Nombre     string
+	Correo     string
+	Contraseña string
+	Genero     string
+}
