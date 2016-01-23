bcb41921ea3d1d464d982b2bb47c566eb5d84da4
Mario <mcarrillo.05@hotmail.com>
2016-01-23 12:57:57 -0600

    Modificar, ConsultarColores y consulta porID para color

--- src/kentia/modelo/color.go
+++ src/kentia/modelo/color.go
@@ -8,7 +8,7 @@ import (
 
 //Color es la estructura que definen los colores de la prenda
 type Color struct {
-	ID     bson.ObjectId `bson:"_id" form:"id"`
+	ID     bson.ObjectId `bson:"_id"`
 	Nombre string
 }
 
@@ -19,6 +19,42 @@ func (c *Color) Registrar() bool {
 	conn := conectar()
 	defer conn.desconectar()
 	err := conn.db.C(coleccionColor).Insert(c)
+	if err != nil {
+		log.RegistrarError(err)
+		return false
+	}
+	return true
+}
+
+//Modificar se encarga de modificar el color en la BD
+func (c *Color) Modificar() bool {
+	conn := conectar()
+	defer conn.desconectar()
+	err := conn.db.C(coleccionColor).UpdateId(c.ID, c)
+	if err != nil {
+		log.RegistrarError(err)
+		return false
+	}
+	return true
+}
+
+//ConsultarColores regresa un catálogo de colores
+func ConsultarColores() (colores []Color) {
+	conn := conectar()
+	defer conn.desconectar()
+	err := conn.db.C(coleccionColor).Find(bson.M{}).All(&colores)
+	if err != nil {
+		log.RegistrarError(err)
+		return colores
+	}
+	return colores
+}
+
+//BuscarPorID busca en la BD un color que coincida con el ID dado
+func (c *Color) BuscarPorID() bool {
+	conn := conectar()
+	defer conn.desconectar()
+	err := conn.db.C(coleccionColor).FindId(c.ID).One(c)
 	if err != nil {
 		log.RegistrarError(err)
 		return false
